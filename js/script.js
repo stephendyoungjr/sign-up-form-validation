3dc3974f1353e007b6a54d25ee247dd733b07319
stephendyoungjr <stephenyoung.np@outlook.com>
2026-09-25 13:54:27 -0400

    event listener for Other selection

--- js/script.js
+++ js/script.js
@@ -2,3 +2,23 @@
 const nameInput = document.getElementById('name');
 nameInput.focus();
 
+
+//Job Role Section 
+//select title by id for ascending changes
+const title = document.getElementById('title');
+const otherJobRole = document.getElementById('other-job-role');
+
+//other job role hidden set to true, making it hidden by defualt unless changed
+otherJobRole.hidden = true;
+
+
+//Add event listener for title change by target
+title.addEventListener('change', (e) => {
+    const targetVal = e.target.value;
+    if (targetVal === 'other') {
+        otherJobRole.hidden = false;
+    } else {
+        otherJobRole.hidden = true;
+    }
+
+})
